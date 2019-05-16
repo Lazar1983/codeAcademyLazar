37877e99269d99fb573b8fff2ccec3b402110dba
Lazar <lazov_lazar@yahoo.com>
2019-05-16 14:08:19 +0200

    javascript

--- JavaScript/nodeIntroMusic/nodeIntroMusic/public/scripts/entities/artist.js
+++
@@ -1,6 +0,0 @@
-export function Artist(data) {
-    this.name = data.name;
-    this.biography = data.bio;
-    this.statistics = data.stats;
-    this.image = data.image[2]["#text"]
-}
--- JavaScript/nodeIntroMusic/nodeIntroMusic/public/scripts/index.js
+++
@@ -1,8 +0,0 @@
-import { Renderer } from "./presentation-layer/artist-render.js";
-
-function main() {
-    var renderer = new Renderer();
-    renderer.init("Ceca");
-}
-
-main();
